31ffc49383f0de5f31bbbf64b93baeb56c888590
osjupiter <water_totuka@yahoo.co.jp>
2016-06-09 21:02:36 +0900

    udas

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # required package: inotify-tools
 
 events=(-e CREATE -e MODIFY -e MOVED_TO)
-while inotifywait ${events[@]} "./"; do
-    go test ./ -v
-    go run ../main.go 
+while inotifywait ${events[@]} -r "./"; do
+    go test ./proAt -v
+    go run ./main.go 
 done
